423f0945b74cf30e2ba80f2c0cd23ca8b448c943
alex <shu5388369@gmail.com>
2018-07-17 19:28:43 +0300

    try 3

--- board.rb
+++ board.rb
@@ -5,19 +5,109 @@ require_relative 'dealer'
 require_relative 'gambler'
 
 class Board
-  attr_reader :result
-  DEAD_HEAT = '---НИЧЬЯ---'
-  WIN = '---ВЫ ВЫИГРАЛИ---'
-  LOSING = '---ВЫ ПРОИГРАЛИ---'
-  TWENTY_ONE = 21
-  
+  attr_reader :result, :data_to_show
+
+  DEAD_HEAT = '---НИЧЬЯ---'.freeze
+  WIN = '---ВЫ ВЫИГРАЛИ---'.freeze
+  LOSING = '---ВЫ ПРОИГРАЛИ---'.freeze
+  TWENTY_ONE = 21.freeze
+  PLAY_GAME = 1.freeze
+  EXIT = 0.freeze
+
+  SKIP_MOVE = 1.freeze
+  TAKE_CARD = 2.freeze
+  OPEN_CARDS = 0.freeze
+
+  START_MENU = [
+    'Выберите пункт меню',
+    '1. Сыграть игру',
+    '0. Выход'
+  ].freeze
+
+  GAME_MENU = [
+    '*****Выберите пункт меню*****',
+    '1. Пропустить ход',
+    '2. Взять карту',
+    '0. Открыть карты'
+  ].freeze
+
   def initialize(params = {})
     @user = User.new(params[:name])
     @deck = Deck.new
     @dealer = Dealer.new('dealer')
     @bank = 0
+    @data_to_show = []
   end
+
+  def do_first_action
+    loop do
+      add_data_to_show(START_MENU)
+
+      index = yield
+      case index
+      when PLAY_GAME
+        play_game do |&block|
+          yield block
+        end
+      when EXIT
+        break
+      end
+    end
+  end
+
+  def clear_data_to_show
+    @data_to_show = []
+  end
+
+  private
   
+  attr_reader :user, :deck, :dealer, :bank
+  attr_writer :bank
+
+  def play_game
+    start
+    add_data_to_show('Игра началась')
+    private_gamers_info
+
+    game_menu do |&block|
+      add_data_to_show(GAME_MENU)
+
+      yield block
+    end
+  end
+
+  def game_menu
+    loop do
+      index = yield
+
+      case index
+        when SKIP_MOVE
+          skip_move
+
+          add_data_to_show('Вы пропустили ход')
+          add_data_to_show(dealer.to_s)
+        when TAKE_CARD
+          take_user_card
+
+          add_data_to_show('Карты вскрыты. Результат раздачи:')
+          public_gamers_info
+
+          summarizing
+
+          add_data_to_show(result)
+          break
+        when OPEN_CARDS
+          add_data_to_show('Карты вскрыты. Результат раздачи:')
+          public_gamers_info
+
+          summarizing
+
+          add_data_to_show(result)
+          break
+      end
+    end
+  end
+
   def start
     @bank = 0
     [user, dealer].each do |player|
@@ -26,32 +116,29 @@ class Board
       @bank += player.place_bet(Gambler::BET)
     end
   end
-  
-  def open_cards
-    summarizing
-  end
-  
+
   def skip_move
     dealer_move
   end
-  
-  def show_user
-    user.to_s
-  end
 
-  def show_dealer
-    dealer.to_s
-  end
-  
   def take_user_card
     take_card(user)
     dealer_move
   end
-  
-  private
-  
-  attr_reader :user, :deck, :dealer, :bank
-  attr_writer :bank
+
+  def private_gamers_info
+    add_data_to_show(user.to_s)
+    add_data_to_show(dealer.to_s)
+  end
+
+  def public_gamers_info
+    add_data_to_show(user.summary_information)
+    add_data_to_show(dealer.summary_information)
+  end
+
+  def add_data_to_show(str)
+    data_to_show << str
+  end
 
   def take_card(gambler)
     cards = deck.give_cards(1)
@@ -93,7 +180,6 @@ class Board
   def you_won
     user.take_bet(bank)
     do_users do |user|
-      puts user.summary_information
       user.discard_cards
     end
     @result = WIN
@@ -102,7 +188,6 @@ class Board
   def you_lose
     dealer.take_bet(bank)
     do_users do |user|
-      puts user.summary_information
       user.discard_cards
     end
     @result = LOSING
@@ -112,7 +197,6 @@ class Board
     user.take_bet(Gambler::BET)
     dealer.take_bet(Gambler::BET)
     do_users do |user|
-      puts user.summary_information
       user.discard_cards
     end
     @result = DEAD_HEAT
--- interface.rb
+++ interface.rb
@@ -1,84 +1,18 @@
 require_relative 'board'
 
 class Interface
-  PLAY_GAME = 1
-  EXIT = 0
-
-  SKIP_MOVE = 1
-  TAKE_CARD = 2
-  OPEN_CARDS = 0
-  
   attr_reader :board
 
-  def show_start_menu
-    puts 'Выберите пункт меню'
-    puts '1. Сыграть игру'
-    puts '0. Выход'
-  end
-
-  def show_game_menu
-    puts '*****Выберите пункт меню*****'
-    puts '1. Пропустить ход'
-    puts '2. Взять карту'
-    puts '0. Открыть карты'
-  end
-  
   def runner
     puts 'Введите имя пользователя'
     name = gets.chomp
     @board = Board.new(name: name)
+    board.do_first_action do
+      puts '------------------'
+      puts board.data_to_show
+      board.clear_data_to_show
 
-    loop do
-      show_start_menu
-  
-      index = gets.chomp.to_i
-      case index
-      when PLAY_GAME then play_game
-      when EXIT then break
-      end
-    end
-    
-  end
-
-  def open_cards
-    puts '__________________________________'
-    puts 'Карты вскрыты. Результат раздачи:'
-    board.open_cards
-    puts board.result
-    puts '__________________________________'
-  end
-  
-  def take_card
-    board.take_user_card
-    puts board.show_user
-    puts board.show_dealer
-  end
-  
-  def skip_move
-    puts 'Вы пропустили ход'
-    board.skip_move
-    puts board.show_dealer
-  end
-  
-  def play_game
-    puts 'Игра началась'
-    board.start
-    puts board.show_user
-    puts board.show_dealer
-
-    loop do
-      show_game_menu
-      index = gets.chomp.to_i
-      case index
-      when SKIP_MOVE then skip_move
-      when TAKE_CARD
-        take_card
-        open_cards
-        break
-      when OPEN_CARDS
-        open_cards
-        break
-      end
+      gets.chomp.to_i
     end
   end
 end
